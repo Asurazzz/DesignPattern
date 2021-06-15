d01f7cb206cd0c37255770a8d8ee523fdc389bfc
ymj <2513667783@qq.com>
2021-06-15 18:21:05 +0800

    增加=> 工厂模式结合策略模式

--- src/com/ymj/pattern/code06_Strategy/PromotionStrategyFactory.java
+++ src/com/ymj/pattern/code06_Strategy/PromotionStrategyFactory.java
@@ -1,5 +1,8 @@
 package com.ymj.pattern.code06_Strategy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Classname PromotionStrategyFactory
  * @Description TODO
@@ -7,4 +10,25 @@ package com.ymj.pattern.code06_Strategy;
  * @Created by yemingjie
  */
 public class PromotionStrategyFactory {
+    private static Map<String,PromotionStrategy> PROMOTION_STRATEGY_MAP = new HashMap<String, PromotionStrategy>();
+    private static final PromotionStrategy NON_PROMOTION = new EmptyStrategy();
+
+    private interface PromotionKey{
+        String COUPON = "COUPON";
+        String CASHBACK = "CASHBACK";
+        String GROUPBUY = "GROUPBUY";
+    }
+    static {
+        PROMOTION_STRATEGY_MAP.put(PromotionKey.COUPON,new CouponStrategy());
+        PROMOTION_STRATEGY_MAP.put(PromotionKey.CASHBACK,new CashbackStrategy());
+        PROMOTION_STRATEGY_MAP.put(PromotionKey.GROUPBUY,new GroupbuyStrategy());
+    }
+
+    private PromotionStrategyFactory(){}
+
+    public static PromotionStrategy getPromotionStrategy(String promotionKey){
+        PromotionStrategy promotionStrategy = PROMOTION_STRATEGY_MAP.get(promotionKey);
+        return promotionStrategy == null ? NON_PROMOTION : promotionStrategy;
+    }
+
 }
---
+++ src/com/ymj/pattern/code06_Strategy/StrategyFactoryTest.java
@@ -0,0 +1,15 @@
+package com.ymj.pattern.code06_Strategy;
+
+/**
+ * @Classname StrategyFactoryTest
+ * @Description TODO
+ * @Date 2021/6/15 18:11
+ * @Created by yemingjie
+ */
+public class StrategyFactoryTest {
+    public static void main(String[] args) {
+        String promotionKey = "GROUPBUY";
+        PromotionActivity promotionActivity = new PromotionActivity(PromotionStrategyFactory.getPromotionStrategy(promotionKey));
+        promotionActivity.execute();
+    }
+}
